1906bedca8c994edc850e53c5a3a8a9b0a159cc0
Aldous Omictin <barbarianclaw1@gmail.com>
2024-09-23 00:36:39 +0800

    template added

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,173 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage("lib/Assets/testIMG2.jpeg"),
-            fit: BoxFit.cover,
-            )),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            // Background image
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("lib/Assets/testIMG2.jpeg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            // Login form
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // App Logo or Title
+                    const Text(
+                      "Travel Assistant",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Username field
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black87.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.only(left: 15, right: 18),
+                            child: CircleAvatar(
+                              radius: 17,
+                              backgroundColor: Colors.white,
+                              child: Icon(Icons.person, color: Colors.black),
+                            ),
+                          ),
+                          hintText: 'Username',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Password field
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black87.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const TextField(
+                          decoration: InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.only(left: 15, right: 18),
+                            child: CircleAvatar(
+                              radius: 17,
+                              backgroundColor: Colors.white,
+                              child: Icon(Icons.lock, color: Colors.black),
+                            ),
+                          ),
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.white70),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Sign In Button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 80,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Handle sign-in
+                      },
+                      child: const Text(
+                        'SIGN IN',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    // Social media login options
+                    const Text(
+                      "Login with your account via social",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.facebook),
+                          color: Colors.blue,
+                          iconSize: 40,
+                          onPressed: () {
+                            // Handle Facebook login
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.g_translate),
+                          color: Colors.red,
+                          iconSize: 40,
+                          onPressed: () {
+                            // Handle Google login
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.alternate_email),
+                          color: Colors.blueAccent,
+                          iconSize: 40,
+                          onPressed: () {
+                            // Handle Twitter login
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.camera_alt),
+                          color: Colors.pinkAccent,
+                          iconSize: 40,
+                          onPressed: () {
+                            // Handle Instagram login
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    // Sign Up Button
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Colors.orange),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 80,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Handle sign-up
+                      },
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(fontSize: 16, color: Colors.orange),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
