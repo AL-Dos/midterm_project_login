2dd54713c62c3779195d9cbd0cd285b53040e90e
AL-Dos <barbarianclaw1@gmail.com>
2024-10-11 01:11:49 +0800

    feat: favorite icon

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -14,16 +14,17 @@ class Dashboard extends StatelessWidget {
         children: [
           SingleChildScrollView(
             child: LayoutBuilder(builder: (context, constraints) {
-              return Center(
-                child: Column(
-                  children: [
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: meals.length,
-                      itemBuilder: (context, index) {
-                        final meal = meals[index];
-                        return Center(
-                          child: Card(
+              return Column(
+                children: [
+                  Stack(
+                    children: [
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: meals.length,
+                        itemBuilder: (context, index) {
+                          final meal = meals[index];
+                          return Card(
                             shadowColor: Colors.yellowAccent,
                             elevation: 5,
                             margin: const EdgeInsets.all(10),
@@ -36,35 +37,44 @@ class Dashboard extends StatelessWidget {
                                 mainAxisSize: MainAxisSize.min,
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  Image.network(
-                                    meal['strMealThumb'],
-                                    height: 400,
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: Image.network(
+                                      meal['strMealThumb'],
+                                      height: 400,
+                                      width: 700,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                   const SizedBox(height: 10),
                                   Text(
                                     meal['strMeal'],
                                     style: const TextStyle(
-                                      fontSize: 25,
+                                      fontSize: 30,
                                       fontWeight: FontWeight.bold,
                                     ),
                                     textAlign: TextAlign.center,
                                   ),
                                   const SizedBox(height: 5),
-                                  Text(
-                                    meal['strCategory'],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.grey,
-                                    ),
-                                    textAlign: TextAlign.center,
+                                  const Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'How to Cook',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                    ],
                                   ),
                                   const SizedBox(height: 5),
                                   Text(
                                     meal['strInstructions'],
                                     style: const TextStyle(
                                       fontSize: 15,
+                                      height: 2,
                                       color: Colors.black87,
                                     ),
                                     textAlign: TextAlign.justify,
@@ -72,30 +82,49 @@ class Dashboard extends StatelessWidget {
                                 ],
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.lightGreen,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        minimumSize: const Size(290, 50),
+                          );
+                        },
                       ),
-                      onPressed: () {
-                        Get.back();
-                      },
-                      child: const Text(
-                        'Logout',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      Positioned(
+                          right: 20,
+                          top: 410,
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.black, width: 2),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: IconButton(
+                              onPressed: () {},
+                              padding: EdgeInsets.zero,
+                              icon: const Icon(Icons.favorite,
+                                  color: Colors.red, size: 55),
+                            ),
+                          )),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
+                      minimumSize: const Size(290, 50),
                     ),
-                    const SizedBox(height: 20)
-                  ],
-                ),
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text(
+                      'Logout',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 20)
+                ],
               );
             }),
           ),
